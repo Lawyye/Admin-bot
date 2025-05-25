b3487ec394a6e043967d403c50a513e22f90ec31
Lawyye <103294499+Lawyye@users.noreply.github.com>
2025-05-25 21:31:47 +0400

    Update legalbot.py

--- legalbot.py
+++ legalbot.py
@@ -304,7 +304,7 @@ async def choose_lang(message: types.Message, state: FSMContext):
     try:
         await bot.send_photo(
             chat_id=message.chat.id,
-            photo="https://imgur.com/a/QSMiNeB",
+            photo="https://i.imgur.com/HDFlGu5.png",
             caption=translations[lang]['welcome'],
             reply_markup=get_menu_kb(user_id, lang)
         )
