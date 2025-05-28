7c1dcfbd19f3974c67f98ff932b72e11fa299fa7
Lawyye <103294499+Lawyye@users.noreply.github.com>
2025-05-28 07:42:59 +0400

    Update legalbot.py

--- legalbot.py
+++ legalbot.py
@@ -4,7 +4,7 @@ from datetime import datetime, timezone
 import sqlite3
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
-from aiogram.filters.text import Text
+from aiogram.filters import Text
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
